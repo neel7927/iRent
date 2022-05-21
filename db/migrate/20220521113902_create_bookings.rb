8b5e67d061be03f8363721ad5a5e627c8f04532c
neelesh <neel7927@gmail.com>
2022-05-21 13:07:50 +0100

    changes to booking migration file

--- db/migrate/20220521113902_create_bookings.rb
+++ db/migrate/20220521113902_create_bookings.rb
@@ -3,8 +3,8 @@ class CreateBookings < ActiveRecord::Migration[6.1]
     create_table :bookings do |t|
       t.references :user, null: false, foreign_key: true
       t.references :item, null: false, foreign_key: true
-      t.Date :start_date
-      t.Date :end_date
+      t.datetime :start_date
+      t.datetime :end_date
       t.decimal :total
       t.boolean :is_accepted
 
