04bca7574ed73723023820907cc4987647085806
Mun Kadell <kadellppsay@gmail.com>
2022-05-27 06:53:46 +0400

    Items controller and Items new page created

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,53 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def home
   end
 
   def index
+    @items = Item.all
   end
 
   def show
   end
 
   def new
+    @item =  Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def destroy
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
   end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:description,:price,:category,:item_condition)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
