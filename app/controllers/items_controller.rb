f2210d3df8d8456b6f3d967f9776978bea70c472
Sajjaad <sajjaadsahodeea@gmail.com>
2022-05-28 12:46:58 +0400

    Home page done - added the categories section

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,16 @@ class ItemsController < ApplicationController
   end
 
   def index
-    @items = Item.all
+    if params[:category]
+      @items = Item.where(category: params[:category])
+      if @items.empty?
+        flash[:error] = "There are <b>#{@items.count}</b> in this category".html_safe
+      else
+        flash[:notice] = "There are <b>${@items.count}</b> in this category".html_safe
+      end
+    else
+      @items = Item.all
+    end
   end
 
   def show
