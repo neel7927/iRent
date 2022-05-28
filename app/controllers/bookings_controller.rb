81033ad056809cc45e9154f833bf3977898a2d1f
neelesh <neel7927@gmail.com>
2022-05-28 09:42:32 +0100

    added bookings listings only for current_user

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,7 @@ class BookingsController < ApplicationController
   before_action :set_booking, only: [:show,:edit,:update]
 
   def index
-    @bookings = Booking.all
+    @bookings = current_user.bookings
   end
 
   def show
