a9f12a4598d9b404e22e5970e1278bc80023ea54
neelesh <neel7927@gmail.com>
2022-05-28 15:55:31 +0100

    fixed all isssues

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'reviews/edit'
-  get 'reviews/destroy'
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/edit'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'items/home'
-  get 'items/index'
-  get 'items/show'
-  get 'items/new'
-  get 'items/create'
-  get 'items/edit'
-  get 'items/destroy'
+  resources :items
+  resources :bookings, only: [:create, :index, :show, :new, :edit, :update]
+  resources :reviews, only: [:create, :destroy, :show, :new, :edit, :update]
   get 'about', to: 'pages#about'
   devise_for :users
   root to: 'pages#home'
