919c397572a8dba353560e005fc09609708e155b
ibrahim peerbux <ibrahim.peerbux28@gmail.com>
2022-06-02 22:15:58 +0400

    new_booking for item

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,7 +13,7 @@ class BookingsController < ApplicationController
 
   def update
     if @booking.update(booking_params)
-      redirect_to booking_path(@booking)
+      redirect_to item_booking_path(@booking)
     else
       render :new
     end
@@ -25,8 +25,14 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @item = Item.find(params[:item_id])
+    @booking.item = @item
+    book_start_date =  Date.parse(params[:booking][:start_date])
+    book_end_date =  Date.parse(params[:booking][:end_date])
+    @booking.total = @booking.item.price * (book_end_date - book_start_date).to_i
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to bookings_path
     else
       render :new
     end
