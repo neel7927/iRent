51da795be9a5dbd2884dd0a23649355140059e22
Mun Kadell <kadellppsay@gmail.com>
2022-06-02 17:24:46 +0400

    added changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :bookings, only: [:create, :index, :show, :new, :edit, :update]
   resources :reviews, only: [:create, :destroy, :show, :new, :edit, :update]
   get 'about', to: 'pages#about'
-  get 'items/:id/edit', to: 'items#edit'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
