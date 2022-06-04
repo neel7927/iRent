32490ddf4c3dda0c37b67a28741de2eab0d9f52f
Sajjaad <sajjaadsahodeea@gmail.com>
2022-06-04 13:23:13 +0400

    Created filter for manage items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,11 +8,13 @@ class ItemsController < ApplicationController
   def index
     if params[:category]
       @items = Item.where(category: params[:category])
-      if @items.empty?
-        flash[:error] = "There are <b>#{@items.count}</b> in this category".html_safe
-      else
-        flash[:notice] = "There are <b>#{@items.count}</b> in this category".html_safe
-      end
+      # if @items.empty?
+      #   flash[:error] = "There are <b>#{@items.count}</b> in this category".html_safe
+      # else
+      #   flash[:notice] = "There are <b>#{@items.count}</b> in this category".html_safe
+      # end
+    elsif params[:user_id]
+      @items = Item.where(user_id: params[:user_id])
     else
       @items = Item.all
     end
